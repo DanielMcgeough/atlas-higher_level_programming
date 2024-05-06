483b81c8218645c05075182f080e0cd44cfde858
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-06 23:34:45 +0000

    minus instead of equals bruh

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    arg_ct - len(sys.argv) - 1
+    arg_ct = len(sys.argv) - 1
     if arg_ct == 0:
         print(arg_ct)
     elif arg_ct == 1:
