db1842ea8ea3ef43cd2d3252f6c90f8df7307e77
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-07 04:18:45 +0000

    whitespace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
     list_len = len(my_list)
-    
+
     for i in range(list_len):
         if (my_list[i] == search):
             new_list[i] = replace
