52e5b6f2e1f6c92a1b72b8bc70187a940d6b9c88
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-07 04:08:03 +0000

    this is new

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,8 @@
 def squareit(number):
     result = number ** 2
     return result
+
+    
 def square_matrix_simple(matrix=[]):
     newMatrix = [list(map(squareit, row)) for row in matrix]
     return newMatrix
