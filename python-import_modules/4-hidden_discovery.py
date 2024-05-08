1ffecff27eb82dee91bf39445e82562723a75a6b
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-08 01:15:01 +0000

    concernicus

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    srt_names - sorted(name for name in names if not name.startswith("__"))
-    for name in srt_names:
-        print(name)
+    for i in dir(hidden_4):
+        if i[0] != "_":
+            print("{}".format(i))
