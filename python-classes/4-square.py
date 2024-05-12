4de101f969f44977a240397bfd79cf31bb978145
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-12 21:34:13 +0000

    getting hella complex lol!

---
+++ python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""Module defines class Square."""
+
+
+class Square:
+    """Class represents a square.
+
+    Attributes:
+        size (int): length of each side."""
+
+    def __init__(self, size=0):
+        """Initialize new Square instance."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Method calculates the area of a Square"""
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
