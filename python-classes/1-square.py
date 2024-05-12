f825cb99406614316d7a1744249f598a0d1518b2
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-12 20:57:35 +0000

    just adding an attribute or field.

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+	"""Defines a square."""
+
+	def __init__(self, size):
+		"""constructor with an argument of size of square."""
+		self.__size = size
