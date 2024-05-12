248d416f0184780cde0965ac815bfb01dc853db7
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-12 22:16:51 +0000

    rework.

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -12,42 +12,33 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Initialize new Square instance."""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size 
-
-        if not isinstance(position, tuple) or not isinstance(position[0], int) or not isinstance(position[1], int) or position[0] < 0 or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        
-        else:
-            self.__position = position
+        self.size = size
+        self.position - position
 
     @property
     def size(self):
         return self.__size
 
     @size.setter
-    def size(self, size):
-        if not isinstance(size, int):
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     @property
     def position(self):
-        return self.__position
+        return (self.__position)
     
     @position.setter
-    def position(self, position):
-        if not isinstance(position, tuple) or not isinstance(position[0], int) or not isinstance(position[1], int) or position[0] < 0 or position[1] < 0:
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+            len(value) != 2 or
+            not all(isinstance(num, int) for num in value) or
+            not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers.")
-        
-        else:
-            self.__position = position
+        self.__position = value
 
     def area(self):
         """Method calculates the area of new instance Square."""
@@ -56,11 +47,11 @@ class Square:
     def my_print(self):
         """Method prints instance of Square represented by '#'."""
         if self.__size == 0:
-            print()
-        else:
-            for i in range(0, self.__size):
-                for k in range(0, self.__position[0]):
-                        print(" ", end="")
-                for k in range(0, self.__size):
-                    print("#", end="")
-                print()
+            print("")
+            return
+            
+    [print("") for i in range(0, self.__position[1])]
+    for i in range(0, self.__size):
+        [print(" ", end="") for j in range(0, self.__position[0])]
+        [print("#", end="") for k in range(0, self.__size)]
+        print("")
