b1de537da7ef7cc29a8724d389d38c58f20be772
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-14 16:24:05 +0000

    I hate this.

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -56,13 +56,13 @@ class Rectangle:
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self) -> str:
-        """shows a visual representation of the rectangle"""
+        """presents a diagram of the rectangle defined for an object"""
         if self.__width == 0 or self.__height == 0:
-            return("")
+            return ("")
         rectangle = ""
         for column in range(self.__height):
             for row in range(self.__width):
-                rectangle += "0"
+                rectangle += "#"
             if column < self.__height - 1:
                 rectangle += "\n"
         return (rectangle)
