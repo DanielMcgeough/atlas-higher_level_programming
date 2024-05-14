b3c9c345826a137ec87b91b74225f862d8ad1f52
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-14 03:23:06 +0000

    this sucks.

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -2,22 +2,10 @@
 """This file defines a function that prints a name"""
 
 
-def say_my_name(first_name, last_name):
-    """This function prints a first and last name
-
-    Args:
-        first_name: A first name
-        last_name:  A last name
-    Raises:
-        TypeError: If the first name isn't a sting
-        TypeError: If the last name isn't a string
-    Returns:
-        Prints the name like a champ
-    """
-
+def say_my_name(first_name, last_name=""):
+    """This function prints a name"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print(f"My name is {first_name} {last_name}")
