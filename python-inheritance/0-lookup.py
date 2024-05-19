edbb3c63a3e2be166aa3687d40f934e4d7f8b180
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-19 23:37:59 +0000

    Readme and task 0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+"""
+    This module will return a list of attributes
+    and methods of an object
+"""
+
+def lookup(obj):
+    """This function looks out for all attributes and methods of an object"""
+    return dir(obj)
