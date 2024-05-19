cc6bf87dc7c1e9109cfe6d502c6056d40ada7120
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-19 23:47:34 +0000

    Python knows types like ooooH!

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    This module returns true if object is exactly
+    an instance of the spec class, otherwise false
+"""
+
+
+def is_same_class(obj, a_class):
+    """return ture if exact same, false if not"""
+    return(type(obj) == a_class)
