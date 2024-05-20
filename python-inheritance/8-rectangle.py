4175b17144e188f9917f14bb6fabb3f3c06b0c6a
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-20 00:29:40 +0000

    extra blank line.

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,6 +4,7 @@
 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """a class to define a rectangle with superclass BaseGeometry"""
     def __init__(self, width, height):
