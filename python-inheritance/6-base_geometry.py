340b2ae52da5fcd019637c5b5f2640cbc5c6ffd7
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-20 00:14:09 +0000

    I love country music

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Module that raises exception with some stuff
+"""
+
+
+class BaseGeometry:
+    """class from earlier task no longer empty"""
+    def area(self):
+        """method not implemented yet"""
+        raise Exception("area() is not implemented")
