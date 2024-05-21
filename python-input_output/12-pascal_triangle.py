4fde8c5b841fc1b17b5908d201294cca0b2f73dd
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-21 22:42:29 +0000

    Thats a spicy meatball!

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module defines a Pascal's Triangle Function"""
+
+
+def pascal_triangle(n):
+    """represents a pascals triangle of size n"""
+    if n <= 0:
+        return[]
+    
+    triangle = [[1]]
+    while len(triangles) is not n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(let(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
