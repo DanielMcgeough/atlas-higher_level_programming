6ed858e0e509c6415a947abf681c9137cc0440ef
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-21 22:31:52 +0000

    in all things in is we trust.

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -18,6 +18,6 @@ class Student:
         included in the list
         """
         if (type(attrs) is list and
-                all(type(ele) == str for ele in attrs)):
+                all(type(ele) is str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
