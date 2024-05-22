f71181e9e21b57ea00bb75f4502cf90cd6428a35
DanielMcGeough <daniel.mcgeough@atlasschool.com>
2024-05-22 16:13:33 +0000

    update

--- PLD_Exercises/LibraryManagement.py
+++ PLD_Exercises/LibraryManagement.py
@@ -2,7 +2,7 @@
 """This is a library Module"""
 
 
-class Book(self):
+class Book:
     def __init__(self, title, author, isbn, is_checked_out):
         self.title = title
         self.author = author
@@ -21,21 +21,29 @@ class Book(self):
             return("book is checked out")
         else:
              return("book is not checked out")
-    
-    book1 = ("Croma Venture", "Joel Shepard", 1234, False )
-    book2 = ("Kite Runner", "Khaled Hosseini", 5678, False)
-    book3 = ("Brutal Prince", "Sophie Lark", 91011, False)
-    book4 = ("Aftershokcs", "Marko Kloos", 1231, True)
 
-class Library(self):
-    books = list[Book] []
+class Library:
+    def __init__(self):
+        self.books = []
     def add_book(self, book):
-        books.append(book)
+        self.books.append(book)
     def remove_book(self, book):
-        books.remove(book)
-    def find_book_by_title(self, title)
-        if book.title isinstance (title)
-    if books is []:
-        return ValueError("There needs to be at least one book in library")
-    
-    
+        self.books.remove(book)
+    def find_book_by_title(self, title):
+        for book in self.books:
+            if book.title.lower() == title.lower():
+                return book
+        return None
+    def list_books(self):
+        for book in self.books:
+            print(f"{book.title}, by {book.author}")
+
+    book1 = Book("Croma Venture", "Joel Shepard", 1234, False )
+    book2 = Book("Kite Runner", "Khaled Hosseini", 5678, False)
+    book3 = Book("Brutal Prince", "Sophie Lark", 91011, False)
+    book4 = Book("Aftershocks", "Marko Kloos", 1231, True)
+    L = Library()
+    L.add_book(book1)
+    L.add_book(book2)
+    L.add_book(book3)
+    L.add_book(book4)
