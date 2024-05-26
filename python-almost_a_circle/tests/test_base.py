e22bdbc27da36242f7518af35c617a4c0c5ac868
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-26 22:11:55 +0000

    needs a rework.

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -7,21 +7,3 @@ class TestBase(unittest.TestCase):
     """
         This module defines test methods for the object class Base.
     """
-
-    def test_base_id(self):
-        """Test id stuff"""
-        b1 = Base(12)
-        self.assertEqual(b1.id, 12)
-        b2 = Base()
-        b3 = Base()
-        self.assertEqual(b2.id, 1)
-        self.assertEqual(b3.id, 2)
-    
-    def test_base_id_type(self):
-        """
-            Test ensures id is of type int.
-        """
-        b = Base(1000)
-        b1 = Base()
-        self.assertIsInstance(b.id, int)
-        self.assertIsInstance(b1.id, int)
