d52ea90b3a215101d4a07067cc1770b84fca3deb
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-28 22:51:18 +0000

    This is not my work.

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,65 +1,140 @@
 #!/usr/bin/python3
-
 import unittest
-import os
-import json
 from models.base import Base
-from models.rectangle import Rectangle
 from models.square import Square
+import json
+import inspect
+
+'''
+    Creating test cases for the base module
+'''
 
 
-class Test_Base(unittest.TestCase):
-    """This module defines test methods for the object class Base."""
-    def setUp(self):
-        Base._Base__nb_objects = 0
+class test_base(unittest.TestCase):
+    '''
+        Testing base
+    '''
+    def test_id_none(self):
+        '''
+            Sending no id
+        '''
+        b = Base()
+        self.assertEqual(1, b.id)
 
     def test_id(self):
-        """Test ensures automatic id assignment"""
-        Base._Base__nb_objects = 0
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
+        '''
+            Sending a valid id
+        '''
+        b = Base(50)
+        self.assertEqual(50, b.id)
 
-    def test_base_id_assignment(self):
-        """Test ensures automatic id assignment in multiple instances"""
-        b1 = Base()
-        b2 = Base()
-        self.assertEqual(b2.id, b1.id + 1)
+    def test_id_zero(self):
+        '''
+            Sending an id 0
+        '''
+        b = Base(0)
+        self.assertEqual(0, b.id)
 
-    def test_base_id(self):
-        """Test ensures user specified id functionality"""
-        b1 = Base(89)
-        self.assertEqual(b1.id, 89)    
-    
-    def test_base_id_type(self):
-        """Test ensures id is an int."""
-        b = Base(1000)
-        b1 = Base()
-        self.assertIsInstance(b.id, int)
-        self.assertIsInstance(b1.id, int)
+    def test_id_negative(self):
+        '''
+            Sending a negative id
+        '''
+        b = Base(-20)
+        self.assertEqual(-20, b.id)
 
-    def test_nb_objects_increment(self):
-        """Test ensures __nb_objects increments as expected"""
-        b1 = Base()
-        b2 = Base()
-        self.assertEqual(Base._Base__nb_objects, 2)
+    def test_id_string(self):
+        '''
+            Sending an id that is not an int
+        '''
+        b = Base("Betty")
+        self.assertEqual("Betty", b.id)
 
-    def test_to_json_string_none(self):
-        """Test ensures empty input to to_json_string returns empty list."""
-        self.assertEqual(Base.to_json_string(None), "[]")
-        self.assertEqual(Base.to_json_string([]), "[]")
+    def test_id_list(self):
+        '''
+            Sending an id that is not an int
+        '''
+        b = Base([1, 2, 3])
+        self.assertEqual([1, 2, 3], b.id)
 
-    def test_to_json_string(self):
-        """Test ensures to_json_string returns a list of dictionaries in string format."""
-        dict1 = {"id": 1, "width": 10, "height": 20}
-        dict2 = {"id": 2, "width": 20, "height": 10}
-        dicts = [dict1, dict2]
-        self.assertEqual(Base.to_json_string([dict1]), json.dumps([dict1]))
-        self.assertEqual(Base.to_json_string(dicts), json.dumps(dicts))
-        self.assertIsInstance(Base.to_json_string(dicts), str)
+    def test_id_dict(self):
+        '''
+            Sending an id that is not an int
+        '''
+        b = Base({"id": 109})
+        self.assertEqual({"id": 109}, b.id)
 
-    def test_save_to_file(self):
-        """Test ensures save_to_file writes JSON string to file."""
-        r1 = Rectangle(10, 7, 2, 8)
-        r2 = Rectangle(2,4)
-        Rectangle.save_to_file([r1, r2])
-        self.assertEqual
+    def test_id_tuple(self):
+        '''
+            Sending an id that is not an int
+        '''
+        b = Base((8,))
+        self.assertEqual((8,), b.id)
+
+    def test_to_json_type(self):
+        '''
+            Testing the json string
+        '''
+        sq = Square(1)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string([json_dict])
+        self.assertEqual(type(json_string), str)
+
+    def test_to_json_value(self):
+        '''
+            Testing the json string
+        '''
+        sq = Square(1, 0, 0, 609)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string([json_dict])
+        self.assertEqual(json.loads(json_string),
+                         [{"id": 609, "y": 0, "size": 1, "x": 0}])
+
+    def test_to_json_None(self):
+        '''
+            Testing the json string
+        '''
+        sq = Square(1, 0, 0, 609)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string(None)
+        self.assertEqual(json_string, "[]")
+
+    def test_to_json_Empty(self):
+        '''
+            Testing the json string
+        '''
+        sq = Square(1, 0, 0, 609)
+        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string([])
+        self.assertEqual(json_string, "[]")
+
+
+class TestSquare(unittest.TestCase):
+    """
+    class for testing Base class' methods
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up class method for the doc tests
+        """
+        cls.setup = inspect.getmembers(Base, inspect.isfunction)
+
+    def test_module_docstring(self):
+        """
+        Tests if module docstring documentation exist
+        """
+        self.assertTrue(len(Base.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """
+        Tests if class docstring documentation exist
+        """
+        self.assertTrue(len(Base.__doc__) >= 1)
+
+    def test_func_docstrings(self):
+        """
+        Tests if methods docstring documntation exist
+        """
+        for func in self.setup:
+            self.assertTrue(len(func[1].__doc__) >= 1)
