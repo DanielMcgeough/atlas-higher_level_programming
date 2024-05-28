3e15f41ec7162ec5ea0dfa1171c9dc3bebf20e49
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-05-28 22:54:50 +0000

    revert

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -828,5 +828,15 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(type(s2_dictionary), dict)
         self.assertFalse(s1 == s2)
 
+    # def test_14_1(self):
+    #     """Test for public method to_dictionary with wrong args."""
+
+    #     s = "to_dictionary() takes 1 positional argument but 2 were given"
+    #     with self.assertRaises(TypeError) as x:
+    #         s1 = Square(10, 2, 1, 9)
+    #         s1_dictionary = s1.to_dictionary("Hi")
+    #     self.assertEqual(s, str(x.exception))
+
+
 if __name__ == '__main__':
     unittest.main()
