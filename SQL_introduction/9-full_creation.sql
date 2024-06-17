3cc6bb68066a9a61f32c2697754a5817a67fa0f2
DannyMcgeough <daniel.mcgeough@atlasschool.com>
2024-06-17 11:55:43 -0500

    try it.

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,14 @@
--- create a talbe and add multiple rows to it
--- new rows will have id, name and score as fields
+-- This script creates a second table
 
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE table IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
 INSERT INTO second_table (id, name, score)
-VALUES (1, "John", 10),
-(2, "Alex", 3),
-(3, "Bob", 14).
-(4, "George", 8);
+VALUES
+    (1, "John", 10),
+    (2, "Alex", 3),
+    (3,"Bob", 14),
+    (4, "George", 8);
