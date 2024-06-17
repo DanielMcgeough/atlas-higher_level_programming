1a8a0a14972e7ce461493b261022800d2a45e017
DannyMcgeough <daniel.mcgeough@atlasschool.com>
2024-06-17 11:56:28 -0500

    oof.

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,7 +1,4 @@
--- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
--- New row:
---     id = 89
---     name = Best school
--- The database name will be passed as an argument of the mysql command
+-- This script inserts a new row in a table
 
-INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');
+INSERT INTO first_table (id, name)
+VALUES (89, "Best School");
