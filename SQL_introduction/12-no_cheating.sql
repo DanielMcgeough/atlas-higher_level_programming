35a2b7077a6acdff4fb0c9a61931f7efb415e2dc
DannyMcgeough <daniel.mcgeough@atlasschool.com>
2024-06-17 11:38:38 -0500

    some more.

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- updates Bobs score to 10.
+-- only all to used field
+
+UPDATE second_table SET score=10 WHERE name='Bob';
---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- removes all records with a score of less than or equal to 5
+-- in the second table
+
+DELETE FROM second_table WHERE score <=5;
