9f045b647b8bafc5b7b8f39e581d7f12e9614840
DannyMcgeough <daniel.mcgeough@atlasschool.com>
2024-06-17 11:08:38 -0500

    Two and three.

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- deletes the database hbtn_0c_0 in your server.
+-- if the database doesn't exist, script will not run.
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- lists all of the tables of a database in the server.
+
+SHOW TABLES;
