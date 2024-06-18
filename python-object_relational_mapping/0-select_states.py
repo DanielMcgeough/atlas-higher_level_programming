874012cba094c7aa72abc68e81402961eccb21d5
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-06-18 10:27:22 -0500

    some setup complete.

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This script lists all states from the
+database named 'hbtn_0e_0_usa'
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    This accesses the database to get
+    the states.
+    """
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], dv=argv[3])
+    db_cursor = db_connect.cursor()
+    
