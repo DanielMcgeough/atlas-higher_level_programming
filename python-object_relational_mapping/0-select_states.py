3569d488ce196c19822a52f915586da0b14049f7
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-06-18 10:28:42 -0500

    maybe complete.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -16,4 +16,9 @@ if __name__ == '__main__':
     db_connect = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306, passwd=argv[2], dv=argv[3])
     db_cursor = db_connect.cursor()
-    
+    db_cursor.execute("SELECT * FROM states")
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
