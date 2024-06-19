93b09b3af2da783577166c007b51dc5a9f6e27cc
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-06-18 22:37:47 -0500

    Its like adlibs for coding.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""
-This script lists all states from the
-database named 'hbtn_0e_0_usa'
-"""
-
+""" Write a script that lists all states from the database hbtn_0e_0_usa """
 
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    """
-    This accesses the database to get
-    the states.
-    """
-    db_connect = MySQLdb.connect(
-        host="localhost", user=argv[1], port=3306, passwd=argv[2], dv=argv[3])
-    db_cursor = db_connect.cursor()
-    db_cursor.execute("SELECT * FROM states")
 
-    rows_selected = db_cursor.fetchall()
+def list_states():
+    """ List the states """
 
-    for row in rows_selected:
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    list_states()
