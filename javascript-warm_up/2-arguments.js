5b765043b828f7eeacf6c3bad8bb7502c24c6c57
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-22 13:20:00 -0500

    two vars for that stuff.

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-const argc = process.argv.length;
 
-if (argc > 2) {
-  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
+const myVar = ['No argument', 'Argument found', 'Arguments found'];
+const len = process.argv.length;
+
+if (len > 3) {
+  console.log(myVar[2]);
 } else {
-  console.log('No argument');
+  console.log(myVar[len - 2]);
 }
