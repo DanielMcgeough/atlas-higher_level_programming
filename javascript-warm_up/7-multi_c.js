c8e9138ed3ba5877167efc6438232eb53b84746a
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-22 14:39:13 -0500

    while loop.

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/urs/bin/node
+
+if (!isNaN(Number(process.argv[2]))) {
+    let i = process.argv[2];
+    while (i > 0) {
+        console.log('C is fun');
+        i--;
+    }
+} else {
+    console.log('Missing number of occurences');
+}
