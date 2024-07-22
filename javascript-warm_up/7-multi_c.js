18ad748c217f28f9077b290789c2ddfc870dc18e
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-22 14:44:25 -0500

    indent stuff.

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 if (!isNaN(Number(process.argv[2]))) {
-    let i = process.argv[2];
-    while (i > 0) {
-      console.log('C is fun');
-      i--;
-    }
-  } else {
-    console.log('Missing number of occurences');
+  let i = process.argv[2];
+  while (i > 0) {
+    console.log('C is fun');
+    i--;
   }
+} else {
+  console.log('Missing number of occurences');
+}
