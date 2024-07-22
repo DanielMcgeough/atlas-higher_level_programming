1679e3e50aedeeaf265c4c90a50299eb4f431a6f
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-22 13:39:10 -0500

    stuck

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
 
-console.log(parseInt(process.argv[2]) ? 'My number: ${parseInt(process.argv[2])}' : 'Not a number');
+if (!isNaN(Number(process.argv[2]))) {
+  console.log(`My number: ${process.argv[2]}`);
+} else {
+  console.log('Not a number');
+}
