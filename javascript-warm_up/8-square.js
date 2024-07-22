147404d150322eb4f6e5dd5892bf9905510d851b
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-22 14:46:51 -0500

    Dont get this one at all.

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+if (!isNaN(Number(process.argv[2]))) {
+    let i = process.argv[2];
+    while (i > 0) {
+      let xString = '';
+      let k = process.argv[2];
+      while (k > 0) {
+        xString += 'X';
+        k--;
+      }
+      console.log(`${xString}`);
+      i--;
+    }
+  } else {
+    console.log('Missing size');
+  }
