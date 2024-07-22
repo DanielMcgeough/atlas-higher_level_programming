a8e319ffb22984defd736ec25477f4eea68a7353
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-22 14:48:48 -0500

    indent stuff.

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 
 if (!isNaN(Number(process.argv[2]))) {
-    let i = process.argv[2];
-    while (i > 0) {
-      let xString = '';
-      let k = process.argv[2];
-      while (k > 0) {
-        xString += 'X';
-        k--;
-      }
-      console.log(`${xString}`);
-      i--;
+  let i = process.argv[2];
+  while (i > 0) {
+    let xString = '';
+    let k = process.argv[2];
+    while (k > 0) {
+      xString += 'X';
+      k--;
     }
-  } else {
-    console.log('Missing size');
+    console.log(`${xString}`);
+    i--;
   }
+} else {
+  console.log('Missing size');
+}
