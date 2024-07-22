fcee1ce27b582fa5079a77f5046dc3c2fa2d3eae
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-22 13:44:49 -0500

    what changed?

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -3,6 +3,6 @@
 const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 let i = 0;
-for(i = 0; i < myVar.length; i++) {
-    console.log(myVar[i]);
+for (i = 0; i < myVar.length; i++) {
+  console.log(myVar[i]);
 }
