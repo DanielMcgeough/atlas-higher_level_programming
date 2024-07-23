dc943b323735ebce09a83f1913b75d5c502d2eae
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-22 19:09:12 -0500

    indents

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 function add (a, b) {
-    if (!isNaN(a) && !isNaN(b)) {
-      const sum = a + b;
-      return (sum);
-    } else {
-      return ('NaN');
-    }
+  if (!isNaN(a) && !isNaN(b)) {
+    const sum = a + b;
+    return (sum);
+  } else {
+    return ('NaN');
   }
-  // export the add function
-  exports.add = add;
+}
+// export the add function
+exports.add = add;
