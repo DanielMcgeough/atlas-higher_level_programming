8453911bff019f1edc5a793a15d97df152dc7ca4
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-28 17:04:17 -0500

    fixed indentation.

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const filename = process.argv[2];
 
 fs.readFile(filename, 'utf-8', (err, content) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(content);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(content);
+  }
 });
