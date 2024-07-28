6735ead31147c90648b19a056c4de3f9b01b8951
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-28 17:10:58 -0500

    updated indentation.

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-const { STATUS_CODES } = require('http');
 const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (error, respone) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('code: ${response.statusCode}');
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code: ${response.statusCode}');
+  }
 });
