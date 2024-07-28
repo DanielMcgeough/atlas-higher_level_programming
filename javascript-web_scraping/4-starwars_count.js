6f6e670dac2f50f30157244fab3e748999a4e1a3
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-28 17:45:27 -0500

    oof.

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,20 +2,21 @@
 
 const request = require('request');
 const url = process.argv[2];
-const id = '18';
+const characterId = '18';
+let count = 0;
 
 request.get(url, (error, response, body) => {
-  if (error){
-  console.log(error);
-  } else {
-  const data = JSON.parse(body);
-  data.results.forEach((film) => {
-    film.characters.forEach((character) => {
-        if (character.includes(characterId)) {
+    if (error) {
+      console.log(error);
+    } else {
+      const data = JSON.parse(body);
+      data.results.forEach((film) => {
+        film.characters.forEach((character) => {
+          if (character.includes(characterId)) {
             count += 1;
-        }
-    });
+          }
+        });
+      });
+      console.log(count);
+    }
   });
-  console.log(count);
-  }
-});
