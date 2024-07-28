041bc3c6f1db32e2e584cd72185840375dedd9e4
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-28 17:22:25 -0500

    capital thing.

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieid = process.argv[2];
+const movieId = process.argv[2];
 
 const url = ('https://swapi-api.alx-tools.com/api/films/${movieId}');
 
