308a042bbf5ad24a168ea58a5ea261ab6a934d5d
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-28 17:16:43 -0500

    spelling mistake.

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url, (error, respone) => {
+request.get(url, (error, response) => {
   if (error) {
     console.log(error);
   } else {
